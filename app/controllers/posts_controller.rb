321a347e939d1b847198bef295ada602aabf1374
Robert Faldo <robertfaldo@gmail.com>
2023-03-26 16:34:51 +0100

    attempt

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: 'Post was successfully deleted.'
+  end
+
   private
 
   def post_params
